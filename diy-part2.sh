6ead1d4fb4d87458760970418670f264bf4bf1bd
Jason <77237605+18271211@users.noreply.github.com>
2024-08-29 17:17:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,3 +23,4 @@ find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+make package/mosdns/luci-app-mosdns/compile V=s
