902dc403536163c49f3dbf4e4ac8430287d14de3
Jason <77237605+18271211@users.noreply.github.com>
2024-08-30 16:39:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,6 +22,8 @@
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
+rm -rf feeds/packages/net/adguardhome
+git clone https://github.com/18271211/adguardhome feeds/packages/net/adguardhome
 
 # drop mosdns and v2ray-geodata packages that come with the source
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
