5f619772b3fe8e4005dc16b2664e77b5da727ff9
Jason <77237605+Jio0oiJ@users.noreply.github.com>
2024-09-04 11:48:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,8 +26,8 @@ rm -rf feeds/packages/net/adguardhome
 git clone https://github.com/Jio0oiJ/adguardhome feeds/packages/net/adguardhome
 
 # drop mosdns and v2ray-geodata packages that come with the source
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -rf
+find ./ | grep Makefile | grep mosdns | xargs rm -rf
 
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
