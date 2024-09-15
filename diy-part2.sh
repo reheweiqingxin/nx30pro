2d9ec732247542a8dcbc349fe5237e610ad6990c
Jason <77237605+Jio0oiJ@users.noreply.github.com>
2024-09-16 03:07:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,15 +20,15 @@
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
 
-rm -rf feeds/packages/net/adguardhome
-git clone https://github.com/Jio0oiJ/adguardhome feeds/packages/net/adguardhome
+#rm -rf feeds/packages/net/adguardhome
+#git clone https://github.com/Jio0oiJ/adguardhome feeds/packages/net/adguardhome
 
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/luci-app-mosdns -b v5.3.1 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 curl -o package/mosdns/luci-app-mosdns/root/etc/mosdns/config_custom.yaml https://raw.githubusercontent.com/Jio0oiJ/mosdns_config/master/config_custom.yaml
 
