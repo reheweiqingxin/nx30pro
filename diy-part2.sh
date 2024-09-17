44fa17f1931aa977927835e574a78d7a6e050a72
Jason <77237605+Jio0oiJ@users.noreply.github.com>
2024-09-17 10:55:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,8 +22,8 @@
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
-rm -rf feeds/packages/net/adguardhome
-git clone https://github.com/Jio0oiJ/adguardhome feeds/packages/net/adguardhome
+#rm -rf feeds/packages/net/adguardhome
+#git clone https://github.com/Jio0oiJ/adguardhome feeds/packages/net/adguardhome
 
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
